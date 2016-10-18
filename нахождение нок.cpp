a880c13fd8c8c9904b416904aad4bd56e374adea
GerdoVika <gerdovika@mail.ru>
2016-10-19 01:57:06 +0400

    Add files via upload

---
+++ нахождение нок.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,nok,n;
+    cin>>a>>b;
+    n=a*b;
+    if(a==b){
+        cout<<a;
+    }
+    else{
+        while(a!=0 && b!=0){
+            if(a<b){
+                a=a%b;
+            }
+            else{
+                b=b%a;
+            }
+        }
+    }
+    nod=n/(a+b);
+    cout<<nok;
+    return 0;
+}
