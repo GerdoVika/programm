7b55b76421802f02f78341671c5680ef222d6eb6
GerdoVika <gerdovika@mail.ru>
2016-10-25 23:03:28 +0400

    Add files via upload

--- возведение в степень.cpp
+++ возведение в степень.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a,b;
+    int a,b,n;
     cin>>a>>b;
-    for(int i=1; i<b; i++){
-      a=a*a;}
+    if (b%2==0){
+        while (b>1){
+            a=a*a;
+            b=b/2;
+        }
+    }
+    else{
+        b=b-1;
+        n=a;
+        while (b>1){
+            a=a*a;
+            b=b/2;
+        }
+        a=a*n;
+    }
     cout<<a;
     return 0;
 }
+
