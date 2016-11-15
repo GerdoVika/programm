5364976fa46794c4f9b052d25227005461110e81
GerdoVika <gerdovika@mail.ru>
2016-11-15 18:50:54 +0300

    Add files via upload

---
+++ дз_1.7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int strange_multiply(int a, int b){
+        int sum;
+        if (a>b){
+            for (int i=1; i<a; i++)
+            sum=b+b;
+        }
+        else{
+            for (int i=1: i<b; i++)
+            sum=a+a;
+        }
+        return sum;
+    }
+    cin>>a>>b;
+    cout<<strange_multily;
+    return 0;
+}
