c826a0e9f63f2bc37be7d9dd74258e61f45ad206
GerdoVika <gerdovika@mail.ru>
2017-01-18 23:06:47 +0300

    Add files via upload

--- 6.shedule.cpp
+++ 6.shedule.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int qsort (int*a, int i, int n){
+int qsort (int**a, int i, int n){
     int st=i, f=n, piv;
     piv=a[2][(i+n)/2];
     while (st<f){
@@ -8,20 +8,22 @@ int qsort (int*a, int i, int n){
         st++;
         while (a[2][f]>piv)
         f--;
-        if (a[2][st]>a[f])
-        swap (a[2][st],a[f]);
+        if (a[2][st]>a[2][f])
+        swap (a[2][st],a[2][f]);
     }
     if (i<st)
     return (a,i,st);
     if (n>f)
     return (a,f,n);
-    return*a;
 }      
 int shedule (int n){
-    int a[2][n];
+    int a=new int*[2];
+    for (int i=0; i<2; i++)
+    int a[i]=new int[n];
     for (int i=0; i<n; i++)
     cin>>a[1][i]>>a[2][i];
-    int b[2][n]=qsort (*a,i,n);
+    int i;
+    qsort (**a,i,n);
     int k=0, sum=1;
     for (int i=1; i<n; i++){
         if (a[2][k]<a[1][i])
